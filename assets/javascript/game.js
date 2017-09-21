d6e3c9b0a115313c8e5dd24cd531cc8f4c31b2a3
Brian Jin <brnjin6@gmail.com>
2017-09-20 23:58:08 -0400

    Fully Functional Crystals Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,37 @@
 var crystalTotalNumber;
 var answerNumber;
-var wins;
-var losses; 
+var rubyNumber;
+var diamondNumber;
+var citrineNumber;
+var emeraldNumb
+
+function startUp (){
+	var totalOfGuessedNumber = 0;
+	crystalTotalNumber = 0;
+
+	//variable set for random numbers
+	answerNumber = (Math.floor(Math.random() * 102) +19);
+	console.log(answerNumber);
+
+	rubyNumber = (Math.floor(Math.random() * 12) + 1);
+	diamondNumber = (Math.floor(Math.random() * 12) + 1);
+	citrineNumber = (Math.floor(Math.random() * 12) + 1);
+	emeraldNumber = (Math.floor(Math.random() * 12) + 1);
+
+	//Sets random number to play
+	$('#answerNumb').text(answerNumber);
+	$('#totalScore').text("0")
+};
 
 $(document).ready(function() {
-	wins = 0;
-	losses = 0;
+	var wins = 0;
+	var losses = 0;
 	crystalTotalNumber = 0; 
-	answerNumber = "";
+	answerNumber = 0;
 
 	//Reset when the game is over
-	function startUp (){
-		var totalOfGuessedNumber = 0;
 
-		//variable set for random numbers
-		answerNumber = (Math.floor(Math.random() * 102) +19);
-		console.log(answerNumber);
-		//Sets random number to play
-		$('#answerNumb').text(answerNumber);
-	};
-	function winLoss() {
-		$('#totalScore').text (crystalTotalNumber);
-		if (answerNumber === crystalTotalNumber) {
-		wins++;
-		$('#wins').html("'You have' + wins");
-		alert("You WON!");
-		console.log(answerNumber);
-		console.log(crystalTotalNumber);
-	};
-		if (crystalTotalNumber > answerNumber) {
-		losses ++;
-		alert("You lost!");
-	};
-};
 		//Random number set for crystals
-	var rubyNumber = (Math.floor(Math.random() * 12) + 1);
-	var diamondNumber = (Math.floor(Math.random() * 12) + 1);
-	var citrineNumber = (Math.floor(Math.random() * 12) + 1);
-	var emeraldNumber = (Math.floor(Math.random() * 12) + 1);
 
 	startUp();
 
@@ -63,7 +57,29 @@ $(document).ready(function() {
 		console.log(crystalTotalNumber);
 	winLoss();
 	});
+	function winLoss() {
+		$('#totalScore').text (crystalTotalNumber);
+		if (answerNumber === crystalTotalNumber) {
+			wins = wins + 1;
+			console.log(wins + "here");
+			$('#wins').text("Wins: " + wins);
+			alert("You WON!");
+			$(":reset").crystalTotalNumber = 0;
+			startUp();
+			
+		};
+		if (crystalTotalNumber > answerNumber) {
+			losses = losses + 1;
+			console.log(losses + "there");
+			$('#losses').text("Losses: " + losses);
+			alert("You lost!");
+			$(":reset").crystalTotalNumber = 0;
+			startUp();
+		};
+	};	
 });
+
+
 
 
 
