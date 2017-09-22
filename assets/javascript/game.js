4071f0f95917daca1853d222cfb5b3f86b58d283
Brian Jin <brnjin6@gmail.com>
2017-09-22 15:31:16 -0400

    Items rearranged, background set

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,7 @@ var diamondNumber;
 var citrineNumber;
 var emeraldNumb
 
+// Initial Set up at the beginning and end of game
 function startUp (){
 	var totalOfGuessedNumber = 0;
 	crystalTotalNumber = 0;
@@ -12,7 +13,7 @@ function startUp (){
 	//variable set for random numbers
 	answerNumber = (Math.floor(Math.random() * 102) +19);
 	console.log(answerNumber);
-
+	//Random number set for crystals
 	rubyNumber = (Math.floor(Math.random() * 12) + 1);
 	diamondNumber = (Math.floor(Math.random() * 12) + 1);
 	citrineNumber = (Math.floor(Math.random() * 12) + 1);
@@ -23,15 +24,12 @@ function startUp (){
 	$('#totalScore').text("0")
 };
 
+
 $(document).ready(function() {
 	var wins = 0;
 	var losses = 0;
 	crystalTotalNumber = 0; 
 	answerNumber = 0;
-
-	//Reset when the game is over
-
-		//Random number set for crystals
 
 	startUp();
 
@@ -57,8 +55,12 @@ $(document).ready(function() {
 		console.log(crystalTotalNumber);
 	winLoss();
 	});
+
+	//Win loss counter 
 	function winLoss() {
 		$('#totalScore').text (crystalTotalNumber);
+
+		//If won
 		if (answerNumber === crystalTotalNumber) {
 			wins = wins + 1;
 			console.log(wins + "here");
@@ -68,6 +70,7 @@ $(document).ready(function() {
 			startUp();
 			
 		};
+		//If lost
 		if (crystalTotalNumber > answerNumber) {
 			losses = losses + 1;
 			console.log(losses + "there");
